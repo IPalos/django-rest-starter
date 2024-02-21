30ad517f6218c54d719ad6926401cba513add2e8
IPalos <ignaciopalos.r@gmail.com>
2024-02-20 22:28:42 -0600

    revert wsgi

--- main/wsgi.py
+++ main/wsgi.py
@@ -10,21 +10,9 @@ https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
-from django.http import HttpResponsePermanentRedirect
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "main.settings")
 
-from django.middleware.security import SecurityMiddleware
 
-
-class HTTPSRedirectMiddleware(SecurityMiddleware):
-    def process_request(self, request):
-        if not request.is_secure():
-            # Redirect to HTTPS
-            request_url = request.build_absolute_uri(request.get_full_path())
-            secure_url = request_url.replace('http://', 'https://')
-            return HttpResponsePermanentRedirect(secure_url)
-        return None
 
 application = get_wsgi_application()
-application = HTTPSRedirectMiddleware(application)
