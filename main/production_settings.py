c18814c57d94273e031c410d24e2a391af2ebff0
IPalos <ignaciopalos.r@gmail.com>
2024-02-20 22:42:46 -0600

    add settings in prod_settings

--- main/production_settings.py
+++ main/production_settings.py
@@ -138,3 +138,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+USE_X_FORWARDED_HOST = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
